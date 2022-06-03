7cdf80aa6ee9d6527e044b6ac54d1665bc0df134
AnBD <budanhan6@gmail.com>
2022-06-03 14:09:24 +0700

    up code

---
+++ TollExportSIP/TollExportFIleSIP.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace TollExportSIP
+{
+    partial class TollExportFIleSIP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TollExportFIleSIP));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txt_FileFath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txt_FileFath
+            // 
+            this.txt_FileFath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_FileFath.Location = new System.Drawing.Point(30, 31);
+            this.txt_FileFath.Name = "txt_FileFath";
+            this.txt_FileFath.Size = new System.Drawing.Size(300, 21);
+            this.txt_FileFath.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FloralWhite;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(336, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Chọn folder";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(30, 83);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(394, 207);
+            this.treeView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(116)))), ((int)(((byte)(189)))));
+            this.button2.BackgroundImage = global::TollExportSIP.Properties.Resources.export_87484;
+            this.button2.Location = new System.Drawing.Point(337, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // TollExportFIleSIP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(436, 335);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_FileFath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TollExportFIleSIP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TollExportFileSIP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txt_FileFath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TollExportSIP/TollExportFIleSIP.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TollExportSIP
+{
+    public partial class TollExportFIleSIP : Form
+    {
+        public TollExportFIleSIP()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //openFileDialog1.ShowDialog();
+            folderBrowserDialog1.ShowDialog();
+            txt_FileFath.Text = folderBrowserDialog1.SelectedPath;
+            string path = folderBrowserDialog1.SelectedPath;
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            FileInfo[] filename = dir.GetFiles("*.*");
+
+            DirectoryInfo[] filenaame = dir.GetDirectories("*.*");
+
+
+
+        }
+    }
+}
