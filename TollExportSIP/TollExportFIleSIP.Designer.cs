41bb94359b01cd6f9b1dbd7731adb157cc2d5bb9
AnBD <budanhan6@gmail.com>
2022-06-03 18:15:27 +0700

    update toll

--- TollExportSIP/TollExportFIleSIP.Designer.cs
+++ TollExportSIP/TollExportFIleSIP.Designer.cs
@@ -36,6 +36,12 @@ namespace TollExportSIP
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.button2 = new System.Windows.Forms.Button();
+            this.label_checkbox = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.labelProgess = new System.Windows.Forms.Label();
+            this.checkBoxALL = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -47,7 +53,7 @@ namespace TollExportSIP
             this.txt_FileFath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_FileFath.Location = new System.Drawing.Point(30, 31);
             this.txt_FileFath.Name = "txt_FileFath";
-            this.txt_FileFath.Size = new System.Drawing.Size(300, 21);
+            this.txt_FileFath.Size = new System.Drawing.Size(405, 21);
             this.txt_FileFath.TabIndex = 0;
             // 
             // button1
@@ -56,40 +62,102 @@ namespace TollExportSIP
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(336, 30);
+            this.button1.Location = new System.Drawing.Point(441, 31);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 25);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Chọn folder";
+            this.button1.Text = "Select folder";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // treeView1
             // 
+            this.treeView1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.treeView1.Location = new System.Drawing.Point(30, 83);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(394, 207);
+            this.treeView1.Size = new System.Drawing.Size(499, 207);
             this.treeView1.TabIndex = 2;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(116)))), ((int)(((byte)(189)))));
             this.button2.BackgroundImage = global::TollExportSIP.Properties.Resources.export_87484;
-            this.button2.Location = new System.Drawing.Point(337, 296);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(442, 296);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(87, 34);
             this.button2.TabIndex = 3;
             this.button2.Text = "Export";
             this.button2.UseVisualStyleBackColor = false;
             // 
+            // label_checkbox
+            // 
+            this.label_checkbox.AutoSize = true;
+            this.label_checkbox.Location = new System.Drawing.Point(51, 67);
+            this.label_checkbox.Name = "label_checkbox";
+            this.label_checkbox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label_checkbox.Size = new System.Drawing.Size(18, 13);
+            this.label_checkbox.TabIndex = 4;
+            this.label_checkbox.Text = "All";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(30, 83);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(499, 17);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // labelProgess
+            // 
+            this.labelProgess.AutoSize = true;
+            this.labelProgess.BackColor = System.Drawing.Color.White;
+            this.labelProgess.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProgess.ForeColor = System.Drawing.Color.Black;
+            this.labelProgess.Location = new System.Drawing.Point(251, 83);
+            this.labelProgess.Name = "labelProgess";
+            this.labelProgess.Size = new System.Drawing.Size(20, 17);
+            this.labelProgess.TabIndex = 6;
+            this.labelProgess.Text = "   ";
+            // 
+            // checkBoxALL
+            // 
+            this.checkBoxALL.AutoSize = true;
+            this.checkBoxALL.Location = new System.Drawing.Point(30, 67);
+            this.checkBoxALL.Name = "checkBoxALL";
+            this.checkBoxALL.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxALL.TabIndex = 7;
+            this.checkBoxALL.UseVisualStyleBackColor = true;
+            this.checkBoxALL.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(13, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "@Copyright by Anbd";
+            // 
             // TollExportFIleSIP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.GhostWhite;
-            this.ClientSize = new System.Drawing.Size(436, 335);
+            this.ClientSize = new System.Drawing.Size(561, 335);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBoxALL);
+            this.Controls.Add(this.labelProgess);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label_checkbox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.button1);
@@ -111,6 +179,12 @@ namespace TollExportSIP
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label_checkbox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label labelProgess;
+        private System.Windows.Forms.CheckBox checkBoxALL;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TollExportSIP/TollExportFIleSIP.cs
+++ TollExportSIP/TollExportFIleSIP.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Threading;
 
 namespace TollExportSIP
 {
@@ -16,25 +17,112 @@ namespace TollExportSIP
         public TollExportFIleSIP()
         {
             InitializeComponent();
+            progressBar1.Visible = false;
+            labelProgess.Visible = false;
+            label_checkbox.Visible = false;
+            checkBoxALL.Visible = false;
         }
-
-
         private void button1_Click(object sender, EventArgs e)
         {
-
-            //openFileDialog1.ShowDialog();
+            treeView1.Nodes.Clear();
             folderBrowserDialog1.ShowDialog();
             txt_FileFath.Text = folderBrowserDialog1.SelectedPath;
             string path = folderBrowserDialog1.SelectedPath;
+            string[] arrListStr = path.Split('\\');
+            TreeNode node = new TreeNode();
+            node.Text = arrListStr[arrListStr.Length - 1];
+            treeView1.CheckBoxes = true;
+            checkBoxALL.Checked = false;
+            treeView1.Nodes.Add(node);
+            LoadExplorer(node, path);
 
-            DirectoryInfo dir = new DirectoryInfo(path);
+        }
+        void LoadExplorer(TreeNode root, string pPath)
+        {
+            if(root == null)
+            {
+                return;
+            }
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(pPath);
+                var file = dir.GetFileSystemInfos("*.*");
+                foreach (var item in file)
+                {
+                    if (!backgroundWorker1.IsBusy)
+                    {
+                        backgroundWorker1.RunWorkerAsync();
+                        labelProgess.Show();
+                        progressBar1.Show();
+                    }
+                    if (Directory.Exists(item.FullName))
+                    {
+                        TreeNode node = new TreeNode() { Text = item.Name };
+                        root.Nodes.Add(node);
+                        LoadExplorer(node, item.FullName);
+                    }
+                    else
+                    {
+                        TreeNode node = new TreeNode() { Text = item.Name };
+                        root.Nodes.Add(node);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+        }
 
-            FileInfo[] filename = dir.GetFiles("*.*");
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                Thread.Sleep(10);
+                backgroundWorker1.WorkerReportsProgress = true;
+                backgroundWorker1.ReportProgress(i);
+            }
+        }
 
-            DirectoryInfo[] filenaame = dir.GetDirectories("*.*");
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            labelProgess.Text = e.ProgressPercentage.ToString() + "%";
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar1.Value = 100;
+            progressBar1.Visible = false;
+            labelProgess.Visible = false;
+            label_checkbox.Show();
+            checkBoxALL.Show();
+        }
 
-
-
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            var tree = treeView1;
+            foreach (TreeNode item in tree.Nodes)
+            {
+                item.Checked = !item.Checked;
+                if(item.Nodes.Count > 0)
+                {
+                    foreach (TreeNode item2 in item.Nodes)
+                    {
+                        CheckNode(item2);
+                    }
+                }
+            }
+        }
+        void CheckNode(TreeNode node)
+        {
+            node.Checked = !node.Checked;
+            if (node.Nodes.Count > 0)
+            {
+                foreach (TreeNode item2 in node.Nodes)
+                {
+                    CheckNode(item2);
+                }
+            }
         }
     }
 }
